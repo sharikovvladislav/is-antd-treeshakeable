9f9b4d578791bf22560dec454abb0fde5f2f8f34
Vladislav.Sharikov <vladislav.sharikov@billing.ru>
2019-10-28 23:44:26 +0300

    add antd. is it treeshakeable?

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { notification } from 'antd';
 
 function App() {
+  notification.info({
+    message: 'Hello, world'
+  });
+  
   return (
     <div className="App">
       <header className="App-header">
